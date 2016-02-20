4bce96b84f6cf4e6f63022298c9e9dfa9ef612b3
Nikolay Molchanov <me@kroniak.net>
2016-02-20 18:47:27 +0300

    [added] project and its configuration

---
+++ src/Forecom.Extensions.Ecwid/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Forecom.Extensions.Ecwid")]
+[assembly: AssemblyDescription("C# services to Ecwid API v1 and v3")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Forecom")]
+[assembly: AssemblyProduct("Forecom.Extensions.Ecwid")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("924d346b-90fa-4251-aab2-0cf860540d2d")]
