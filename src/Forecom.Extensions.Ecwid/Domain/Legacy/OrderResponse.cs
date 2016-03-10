127c676e9d29a8ae82e1c68decf8b1c7208d4610
Nikolay Molchanov <me@kroniak.net>
2016-03-10 12:51:24 +0300

    [add] legacy domain classes

---
+++ src/Forecom.Extensions.Ecwid/Domain/Legacy/OrderResponse.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+
+/// <summary>
+/// Lagacy Ecwid API domain model v1
+/// </summary>
+namespace Forecom.Extensions.Ecwid.Domain.Legacy
+{
+    /// <summary>
+    /// The root object that is returned by the Order API
+    /// </summary>
+    internal class OrderResponse
+    {
+        /// <summary>
+        /// The number of orders returned in the 'orders' field
+        /// </summary>
+        /// <value>
+        /// The count
+        /// </value>
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        /// <summary>
+        /// The number of orders satisfying the conditions specified in the request URL
+        /// </summary>
+        /// <value>
+        /// The total
+        /// </value>
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        /// <summary>
+        /// URL of the API request for the next page or null if there if not next page
+        /// </summary>
+        /// <value>
+        /// The next URL
+        /// </value>
+        [JsonProperty("nextUrl")]
+        public string NextUrl { get; set; }
+
+        /// <summary>
+        /// No more then limit orders starting from the given offset ordered by time descending. See <see cref="Order"/>
+        /// </summary>
+        /// <value>
+        /// The orders
+        /// </value>
+        [JsonProperty("orders")]
+        public IList<Order> Orders { get; set; }
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class PaymentParameters
+    {
+    }
+}
--- src/Forecom.Extensions.Ecwid/Properties/AssemblyInfo.cs
+++ src/Forecom.Extensions.Ecwid/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly
 [assembly: AssemblyTitle("Forecom.Extensions.Ecwid")]
 [assembly: AssemblyDescription("C# services to Ecwid API v1 and v3")]
 [assembly: AssemblyConfiguration("")]
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true on that type
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
