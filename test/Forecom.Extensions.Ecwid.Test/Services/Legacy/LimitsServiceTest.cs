41b8a39604fd3f44d2dd2fcc01b21ca9e8c9e3fb
Nikolay Molchanov <me@kroniak.net>
2016-03-11 19:50:20 +0300

    [add] limit for lagacy api [add] tests for limits

---
+++ test/Forecom.Extensions.Ecwid.Test/Services/Legacy/LimitsServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Forecom.Extensions.Ecwid.Services.Legacy;
+using Moq;
+using Xunit;
+
+namespace Forecom.Extensions.Ecwid.Test.Services.Legacy
+{
+    /// <summary>
+    /// Test for Limits functionality of Ecwid Lagacy
+    /// </summary>
+    public class LimitsServiceTest
+    {
+        private LimitsService _limitsService;
+
+        [Fact]
+        public void SimpleTickExpectTrue()
+        {
+            _limitsService = new LimitsService();
+
+            var result = _limitsService.Tick();
+            Assert.True(result);
+        }
+
+        [Fact]
+        // Bug. Test Failed
+        public void TickExpectPass()
+        {
+            // Mock current time
+            var mockTime = new Mock<ITimeProvider>();
+            // Init time
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now);
+
+            // Init service
+            _limitsService = new LimitsService(timeProvider: mockTime.Object);
+
+            var result = new List<bool>();
+
+            // max 1400 in 500 sec
+            for (var a = 1; a <= 3; a++)
+            {
+                // max 400 in 50 sec
+                for (var b = 1; b < 5; b++)
+                {
+                    // max 100 in 5 sec
+                    for (var c = 1; c <= 80; c++)
+                        result.Add(_limitsService.Tick());
+                    //Switch time to + [n] sec.
+                    mockTime.SetupGet(t => t.Now).Returns(DateTime.Now.AddSeconds(6));
+                }
+                //Switch time to + [n] sec.
+                mockTime.SetupGet(t => t.Now).Returns(DateTime.Now.AddSeconds(50));
+            }
+            //Switch time to + [n] sec.
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now.AddSeconds(500));
+
+            // Test again
+            // max 100 in 5 sec
+            for (var c = 1; c <= 80; c++)
+                result.Add(_limitsService.Tick());
+
+            Assert.DoesNotContain(false, result);
+        }
+    }
+}
---
+++ test/Forecom.Extensions.Ecwid.Test/Services/Legacy/OneLimitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Forecom.Extensions.Ecwid.Services.Legacy;
+using Moq;
+using Xunit;
+
+namespace Forecom.Extensions.Ecwid.Test.Services.Legacy
+{
+    /// <summary>
+    /// Test for one limit functionality of Ecwid Lagacy
+    /// </summary>
+    public class OneLimitTest
+    {
+        [Theory]
+        [InlineData(5, 100)]
+        [InlineData(50, 400)]
+        [InlineData(500, 1400)]
+        public void TickExpectPass(int timeInterval, int limitValue)
+        {
+            // Mock current time
+            var mockTime = new Mock<ITimeProvider>();
+            // Init time
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now);
+
+            var limit = new Limit(timeInterval, limitValue) { TimeProvider = mockTime.Object };
+            var results = new List<bool>();
+
+            for (var i = 1; i <= limitValue; i++)
+                results.Add(limit.Tick());
+
+            //Switch time to + [n] sec.
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now.AddSeconds(timeInterval));
+
+            // Check again
+            for (var i = 1; i <= limitValue; i++)
+                results.Add(limit.Tick());
+
+            Assert.DoesNotContain(false, results);
+        }
+
+        [Theory]
+        [InlineData(5, 100, 105)]
+        [InlineData(50, 400, 405)]
+        [InlineData(500, 1400, 2000)]
+        public void TickExpectFail(int timeInterval, int limitValue, int realLimit)
+        {
+            // Mock current time
+            var mockTime = new Mock<ITimeProvider>();
+            // Init time
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now);
+
+            var limit = new Limit(timeInterval, limitValue) { TimeProvider = mockTime.Object };
+            var results = new List<bool>();
+
+            for (var i = 1; i <= limitValue; i++)
+                results.Add(limit.Tick());
+
+            //Switch time to + [n] sec.
+            mockTime.SetupGet(t => t.Now).Returns(DateTime.Now.AddSeconds(timeInterval));
+
+            // Check again
+            for (var i = 1; i <= realLimit; i++)
+                results.Add(limit.Tick());
+
+            Assert.Contains(false, results);
+        }
+    }
+}
