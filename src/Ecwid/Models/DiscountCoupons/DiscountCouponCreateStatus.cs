ebc3cfc647812141a62365aa0272d137ae78b2e1
Alex Allen <alex@koan.is>
2017-11-20 16:36:46 +0000

    Initial implementation of IEcwidDiscountCouponsClient

---
+++ src/Ecwid/Models/DiscountCoupons/DiscountCouponCreateStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace Ecwid
+{
+    /// <summary>
+    ///     The object returned from the Discount Coupon API when creating new Discount Coupons
+    /// </summary>
+    public class DiscountCouponCreateStatus
+    {
+        /// <summary>
+        /// Internal unique coupon Id
+        /// </summary>
+        public long Id { get; set; }
+        
+        /// <summary>
+        /// Code of the created coupon
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
