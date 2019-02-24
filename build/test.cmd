b55cc3530abbd889759676fbb0f3e587f8534572
Nikolay Molchanov <me@kroniak.net>
2019-02-24 19:40:41 +0300

    [edit] add excludes to test.cmd

--- build/test.cmd
+++ build/test.cmd
@@ -1,4 +1,4 @@
-@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=50 ..\test\Ecwid.Test\
+@call dotnet test -c Release /p:CollectCoverage=true /p:Exclude="[xunit.*]*" /p:Threshold=50 ..\test\Ecwid.Test\
 @if ERRORLEVEL 1 (
 echo Error! Tests for Server failed.
 exit /b 1
